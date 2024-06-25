08a8d7d0d60acafc4239cbeaf5f1c39b22b89051
mariavllr <al394488@uji.es>
2024-06-25 14:41:31 +0200

    Mejoras de calidad

--- Assets/Scripts/NoNetwork/ChangeQualitySettings.cs
+++ Assets/Scripts/NoNetwork/ChangeQualitySettings.cs
@@ -6,17 +6,98 @@ using UnityEngine.Rendering;
 
 public class ChangeQualitySettings : MonoBehaviour
 {
-    public RenderPipelineAsset[] qualityLevels;
-    public TMP_Dropdown dropdown;
+    public TMP_Dropdown dropdownQualityLevel, dropdownShadows, dropdownShadowRes, dropdownShadowDistance;
 
-    void Start()
+    void Awake()
     {
-        dropdown.value = QualitySettings.GetQualityLevel();
+        UpdateValues();
     }
 
-    public void ChangeLevel(int value)
+    void UpdateValues()
     {
-        QualitySettings.SetQualityLevel(value);
-        QualitySettings.renderPipeline = qualityLevels[value];
+        dropdownQualityLevel.value = QualitySettings.GetQualityLevel();
+        dropdownShadows.value = (int)QualitySettings.shadows;
+        dropdownShadowRes.value = (int)QualitySettings.shadowResolution;
+
+        switch (QualitySettings.shadowDistance)
+        {
+            case 15:
+                dropdownShadowDistance.value = 0;
+                break;
+            case 20:
+                dropdownShadowDistance.value = 1;
+                break;
+            case 40:
+                dropdownShadowDistance.value = 2;
+                break;
+            case 70:
+                dropdownShadowDistance.value = 3;
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void ChangeQualityLevel()
+    {
+        QualitySettings.SetQualityLevel(dropdownQualityLevel.value, true);
+        UpdateValues();
+    }
+
+    public void ChangeShadows()
+    {
+        switch (dropdownShadows.value)
+        {
+            case 0:
+                QualitySettings.shadows = ShadowQuality.Disable;
+                break;
+            case 1:
+                QualitySettings.shadows = ShadowQuality.HardOnly;
+                break;
+            case 2:
+                QualitySettings.shadows = ShadowQuality.All;
+                break;
+        }
+        UpdateValues();
+    }
+
+    public void ChangeShadowResolution()
+    {
+        switch (dropdownShadowRes.value)
+        {
+            case 0:
+                QualitySettings.shadowResolution = ShadowResolution.Low;
+                break;
+            case 1:
+                QualitySettings.shadowResolution = ShadowResolution.Medium;
+                break;
+            case 2:
+                QualitySettings.shadowResolution = ShadowResolution.High;
+                break;
+            case 3:
+                QualitySettings.shadowResolution = ShadowResolution.VeryHigh;
+                break;
+        }
+        UpdateValues();
+    }
+    
+    public void ChangeShadowDistance()
+    {
+        switch (dropdownShadowDistance.value)
+        {
+            case 0:
+                QualitySettings.shadowDistance = 15;
+                break;
+            case 1:
+                QualitySettings.shadowDistance = 20;
+                break;
+            case 2:
+                QualitySettings.shadowDistance = 40;
+                break;
+            case 3:
+                QualitySettings.shadowDistance = 70;
+                break;
+        }
+        UpdateValues();
     }
 }
