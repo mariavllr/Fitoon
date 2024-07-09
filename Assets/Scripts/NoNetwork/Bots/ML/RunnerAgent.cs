61236917c30bdd91b1bd7544ef5e48c258b17a74
nadiaxm5 <al394716@uji.es>
2024-07-09 08:26:53 +0200

    Arreglar cosas

--- Assets/Scripts/NoNetwork/Bots/ML/RunnerAgent.cs
+++ Assets/Scripts/NoNetwork/Bots/ML/RunnerAgent.cs
@@ -11,7 +11,6 @@ public class RunnerAgent : Agent
     [SerializeField] private bool training = false;
 
     private NPCController controller;
-    //[SerializeField] private CreateBots createBots;
     private Vector3 diff;
     private Vector3 lastDiff;
     [SerializeField] private HashSet<GameObject> checkpointsPassedSet;
@@ -21,17 +20,10 @@ public class RunnerAgent : Agent
         controller = GetComponent<NPCController>();
         checkpointsPassedSet = new HashSet<GameObject>();
         target = FindObjectOfType<GoalController>().transform;
-        //createBots = GetComponentInParent<CreateBots>();
     }
 
     public override void OnEpisodeBegin()
     {
-        //if (training)
-        //{
-        //    transform.localRotation = Quaternion.identity;
-        //    transform.localPosition = new Vector3(Random.Range(-20f, 20f), 1f, 0f);
-        //}
-        //createBots.SpawnBots();
         controller.enabled = true;
         controller.SetBoost(1f);
         target.GetComponent<Collider>().enabled = true;
@@ -45,11 +37,11 @@ public class RunnerAgent : Agent
         diff = (target.transform.localPosition - transform.localPosition) / 20;
         sensor.AddObservation(diff);
 
-        if (diff.magnitude < lastDiff.magnitude) AddReward(0.0001f);
-        if (diff.magnitude > lastDiff.magnitude) AddReward(-0.01f);
+        if (diff.magnitude < lastDiff.magnitude) AddReward(0.1f);
+        if (diff.magnitude > lastDiff.magnitude) AddReward(-0.1f);
         lastDiff = diff;
 
-        AddReward(-0.001f); //Encourage the player to move faster
+        //AddReward(-0.001f); //Encourage the player to move faster
     }
 
     public override void OnActionReceived(ActionBuffers actions)
@@ -59,14 +51,12 @@ public class RunnerAgent : Agent
         float moveH = ScaleAction(actions.ContinuousActions[1], -1.0f, 1.0f);
         controller.SetMovement(moveV, moveH);
 
-        //Penalize for staying still
-        if (moveV <= 0)
+        if (moveV == 0 && moveH == 0)
         {
-            AddReward(-10f);
+            AddReward(-0.1f); // Penalize for staying still
         }
 
-        //Reiniciar si va mal
-        if (GetCumulativeReward() < -2000)
+        if (GetCumulativeReward() < -1000)
         {
             checkpointsPassedSet.Clear();
             EndEpisode();
@@ -85,24 +75,24 @@ public class RunnerAgent : Agent
     {
         if (collider.gameObject.CompareTag("Goal"))
         {
-            AddReward(1000f);
+            AddReward(500f);
             EndEpisode();
         }
         if (collider.gameObject.CompareTag("POI"))
         {
-            AddReward(200f);
+            AddReward(20f);
         }
         if (collider.gameObject.CompareTag("Checkpoint"))
         {
             if (checkpointsPassedSet.Contains(collider.gameObject))
             {
-                AddReward(-100f); // Penalize for reaching a repeated CheckPoint
+                AddReward(-5f); // Penalize for reaching a repeated CheckPoint
                 EndEpisode();
             }
             else
             {
                 checkpointsPassedSet.Add(collider.gameObject);
-                AddReward(100f); // Reward for reaching a new CheckPoint
+                AddReward(50f); // Reward for reaching a new CheckPoint
             }
         }
     }
@@ -113,15 +103,15 @@ public class RunnerAgent : Agent
         {
             if (collision.gameObject.CompareTag("Wall"))
             {
-                AddReward(-10f); //Penalization for colliding with a wall
+                AddReward(-0.1f); // Apply a small penalty continuously
             }
             if (collision.gameObject.CompareTag("Obstacle"))
             {
-                AddReward(-5f); //Penalization for colliding with an obstacle
+                AddReward(-0.05f); // Apply a small penalty continuously
             }
-            if (collision.gameObject.CompareTag("Character"))
+            if (collision.gameObject.CompareTag("Player"))
             {
-                AddReward(-1f); //Penalization for colliding with another NPC too much time
+                AddReward(-0.001f); // Apply a small penalty continuously
             }
         }
     }
